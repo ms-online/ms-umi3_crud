962a7418b45c0bf05cc2ce0897e5a217288e8260
米斯特吴 <309595700@qq.com>
2020-05-11 19:10:04 +0800

    lesson-9

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const index = () => {
-  return <div>关于我们</div>;
+  return (
+    <div>
+      <img width="1800" src={require('@/assets/order.png')} alt="" />
+    </div>
+  );
 };
 
 export default index;
